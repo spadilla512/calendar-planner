cfbfecdb7de54977204350c9177d4328a3afcd82
spadilla512 <spadilla512@gmail.com>
2023-10-29 23:23:01 -0500

    updated script.js file to update the colors

--- assets/script.js
+++ assets/script.js
@@ -1,47 +1,58 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(document).ready(function () {
 
-$("#currentDay").text(moment().format("dddd, MMM DD, YYYY - h:mm:ss a"));
+var localSettings = {};
+dayjs.local(localSettings);
 
-    $("saveBtn").on("click", function() {
-        var content = $(this).siblings(".description").val();
-        var timeRecord = $(this).parent().attr("id");
-
-        localStorage.setItem(timeRecord, content);
-    })
-
-    $("#hour-9.description").val(localStorage.getItem("hour-9"));
-    $("#hour-10.description").val(localStorage.getItem("hour-10"));
-    $("#hour-11.description").val(localStorage.getItem("hour-11"));
-    $("#hour-12.description").val(localStorage.getItem("hour-12"));
-    $("#hour-13.description").val(localStorage.getItem("hour-13"));
-    $("#hour-14.description").val(localStorage.getItem("hour-14"));
-    $("#hour-15.description").val(localStorage.getItem("hour-15"));
-    $("#hour-16.description").val(localStorage.getItem("hour-16"));
-    $("#hour-17.description").val(localStorage.getItem("hour-17"));
+$(function () {
+    var currentHour = dayjs().format("H");
 
     function timeline() {
-        var Workhour = moment().hour();
-
         $(".time-block").each(function() {
-            var timeBlock = parseInt($(this).attr("id").split("hour")[1]);
+            var rowTime = parseInt(this.id);
+            $(this).toggleClass("past", rowTime < currentHour);
+            $(this).toggleClass("present", rowTime === currentHour);
+            $(this).toggleClass("future", rowTime > currentHour);
 
-            if (timeBlock < Workhour) {
-                $(this).addClass("past");  
+            if (rowTime < currentHour) {
+                $(this).addClass("past");
                 $(this).removeClass("present");
                 $(this).removeClass("future");
-            } else if (timeBlock === Workhour) {
+            } else if (rowTime === currentHour) {
                 $(this).removeClass("past");
                 $(this).addClass("present");
                 $(this).removeClass("future");
-            } else {
+            } else (rowTime > currentHour) {
                 $(this).removeClass("past");
                 $(this).removeClass("present");
                 $(this).addClass("future");
             }
-        })
+        });
     }
-        timeline();
-  });
+
+    function userTask () {
+        $(".saveBtn").on("click", function() {
+            var key = $(this).parent().attr("id");
+            var value= $(this).siblings(".description").val();
+            localStorage.setItem(key, value);
+        });
+    }
+
+    $(".time-block").each(function() {
+        var key = $(this).attr("id");
+        var value = localStorage.getItem(key);
+        $(this).children(".description").val(value);
+    });
+
+    function currentTime() {
+        var date = $("#date");
+        var currentDate = dayjs().format("dddd, MMMM D, YYYY");
+        date.text(currentDate);
+    }
+
+    timeline();
+    userTask();
+    setInterval(updateTime, 1000);
+});
+
